bfce5eeb5f62c0e4a1dd1db9f4970995bc393fa9
Mordecai Mengesteab <mgmenges@ncsu.edu>
2025-03-26 19:37:05 -0400

    changed function name to fix api

--- data/doc-retrieval/__index__.py
+++ data/doc-retrieval/__index__.py
@@ -1,5 +1,13 @@
 from fastapi import FastAPI
-from retrieval import health
+from retrieval import retrieve, health
+from pydantic import BaseModel
+
+
+class DocQuery(BaseModel):
+    category: str | None = None
+    query: str
+    top_k: int | None = None  # only works in 3.10 or above
+
 
 app = FastAPI()
 
@@ -10,5 +18,13 @@ async def root():
 
 
 @app.get("/health/{txt}")
-async def retrieve(txt):
+async def health(txt):
     return health(txt)
+
+
+@app.post("/doc-retrieval/")
+async def create_item(body: DocQuery):
+    query = body.query
+    top_k = body.top_k if body.top_k is not None else 1
+    results = retrieve(query, top_k)
+    return {"result": results}
