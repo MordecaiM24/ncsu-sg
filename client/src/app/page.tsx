a5b82ec8e11695de66b9ca2a4dd529c893d15469
Mordecai Mengesteab <mgmenges@ncsu.edu>
2025-04-01 19:19:54 -0400

    initial version of frontend

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,30 +1,132 @@
 "use client";
+import React, { useState } from "react";
+import { Search, PenLine, FileText, Layers, ArrowUp } from "lucide-react";
+import { cn } from "@/lib/utils";
 
-export default function Landing() {
+const HomeUI = () => {
+  const [docSearch, setDocSearch] = useState(false);
+  const [ask, setAsk] = useState(false);
+  const [prompt, setPrompt] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [response, setResponse] = useState();
+
+  async function getDocuments() {
+    setLoading(true);
+
+    const res = await fetch(`http://localhost:8000/doc-retrieval/`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: prompt, top_k: 3 }),
+    });
+
+    const response = await res.json();
+
+    console.log(response);
+    setResponse(response);
+    setLoading(false);
+  }
+
   return (
-    <div className="relative flex w-full items-center py-10 md:py-20">
-      {/* Gradient Background */}
-      <div className="gradient absolute inset-0 top-1/2 left-1/2 -z-10 h-3/4 w-3/4 -translate-x-1/2 -translate-y-1/2 blur-[10rem]"></div>
+    <div className="flex min-h-screen flex-col items-center justify-center bg-black p-4 pb-20 text-white">
+      <div className="mt-16 flex w-full max-w-3xl flex-col items-center">
+        <h1 className="mb-8 text-3xl font-bold">What do you want to know?</h1>
 
-      {/* Container */}
-      <div className="bg-opacity-50 ring-foreground/20 -m-2 rounded-xl p-2 ring-1 backdrop-blur-3xl ring-inset lg:-m-4 lg:rounded-2xl">
-        {/* Black Rectangle */}
-        <div className="bg-black w-[1200px] h-[1200px] ring-border rounded-md ring-1 shadow-2xl lg:rounded-xl"></div>
+        <div className="mb-6 w-full">
+          <form
+            className="flex items-center rounded-full bg-zinc-900 p-3 pl-6"
+            onSubmit={(e) => {
+              console.log("submitted");
+              e.preventDefault();
+              getDocuments();
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Ask anything"
+              className="flex-grow bg-transparent px-2 text-white outline-none"
+              value={prompt}
+              onChange={(e) => {
+                setPrompt(e.target.value);
+                console.log(prompt);
+              }}
+            />
 
-        {/* Animated Border */}
-        <div
-          style={{
-            "--size": 250,
-            "--duration": "12s",
-            "--anchor": 90,
-            "--border-width": 1.5,
-            "--color-from": "#ffaa40",
-            "--color-to": "#9c40ff",
-            "--delay": "-9s",
-          }}
-          className="pointer-events-none absolute inset-0 rounded-[inherit] [border:calc(var(--border-width)*1px)_solid_transparent] [mask-clip:padding-box,border-box] [mask-composite:intersect] [mask:linear-gradient(transparent,transparent),linear-gradient(white,white)] after:animate-border-beam after:absolute after:aspect-square after:w-[calc(var(--size)*1px)] after:[animation-delay:var(--delay)] after:[background:linear-gradient(to_left,var(--color-from),var(--color-to),transparent)] after:[offset-anchor:calc(var(--anchor)*1%)_50%] after:[offset-path:rect(0_auto_auto_0_round_calc(var(--size)*1px))]"
-        />
+            <div className="flex items-center space-x-2">
+              <button
+                type="button"
+                className={cn(
+                  "flex items-center space-x-1 rounded-full px-3 py-2 transition-all duration-75",
+                  ask
+                    ? "border border-blue-500 bg-blue-500/50 text-blue-100"
+                    : "border border-zinc-800 bg-zinc-800",
+                )}
+                onClick={() => setAsk((ask) => !ask)}
+              >
+                <Search size={16} />
+                <span className="text-sm">Ask</span>
+              </button>
+
+              <button
+                type="button"
+                className={cn(
+                  "flex items-center space-x-1 rounded-full bg-zinc-800 px-3 py-2 duration-75",
+                  docSearch
+                    ? "border border-blue-500 bg-blue-500/50 text-blue-100"
+                    : "border border-zinc-800 bg-zinc-800",
+                )}
+                onClick={() => setDocSearch((docSearch) => !docSearch)}
+              >
+                <FileText size={16} />
+                <span className="text-sm">Document Search</span>
+              </button>
+
+              <button
+                disabled={loading}
+                type="submit"
+                className="flex items-center rounded-full bg-white p-2 text-black transition-all hover:cursor-pointer hover:bg-slate-200 active:bg-slate-300 disabled:text-slate-400"
+              >
+                <ArrowUp size={18} />
+              </button>
+            </div>
+          </form>
+        </div>
+
+        <div className="flex flex-wrap justify-center gap-2">
+          <button
+            className="flex items-center space-x-2 rounded-full bg-zinc-900 px-4 py-2 hover:bg-zinc-800"
+            onClick={() => {
+              setPrompt("What bills talk about DEI?");
+            }}
+          >
+            <FileText size={18} className="text-orange-500" />
+            <span className="text-sm">What bills talk about DEI?</span>
+          </button>
+          <button
+            className="flex items-center space-x-2 rounded-full bg-zinc-900 px-4 py-2 hover:bg-zinc-800"
+            onClick={() => {
+              setPrompt("What are the requirements for senators?");
+            }}
+          >
+            <Layers size={18} className="text-blue-500" />
+            <span className="text-sm">
+              What are the requirements for senators?
+            </span>
+          </button>
+          <button
+            className="flex items-center space-x-2 rounded-full bg-zinc-900 px-4 py-2 hover:bg-zinc-800"
+            onClick={() => {
+              setPrompt("Who voted for R29?");
+            }}
+          >
+            <PenLine size={18} className="text-purple-500" />
+            <span className="text-sm">Who voted for R29?</span>
+          </button>
+        </div>
       </div>
+
+      <p>{JSON.stringify(response)}</p>
     </div>
   );
-}
+};
+
+export default HomeUI;
