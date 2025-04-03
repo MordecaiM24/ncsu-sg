52d884967757f609c3b77dd9e13afdd8f8a77a70
Mordecai Mengesteab <mgmenges@ncsu.edu>
2025-04-02 22:08:50 -0400

    fuck cors

--- data/doc-retrieval/__index__.py
+++ data/doc-retrieval/__index__.py
@@ -16,12 +16,11 @@ ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 app = FastAPI()
 
-origins = ["https://sg.m16b.com", "http://localhost:3000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
